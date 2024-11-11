fad357aefddac74701f961773dcfbc54e8bf4d84
WizzyCodes <wisdomiberi832@gmail.com>
2024-11-11 02:25:41 +0100

    just

---
+++ Start_coding/src/Components/LayoutHolder/Layout.tsx
@@ -0,0 +1,15 @@
+import Footer from "../Static/Footer";
+import Header from "../Static/Header";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
